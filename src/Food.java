f968dc1ddf999bb4c5a698ff771a45f8625f59d3
Gabriel Franca <gabrielgfranca7@gmail.com>
2025-10-05 19:45:32 -0300

    Food Class

---
+++ src/Food.java
@@ -0,0 +1,21 @@
+public class Food extends Store {
+    private Date licenseDate;
+
+    public Food(String name, int employeeQuantity, Address address, Date fundationDate, Date licenseDate) {
+        super(name, employeeQuantity, address, fundationDate);
+        this.licenseDate = licenseDate;
+    }
+
+    public Food(String name, int employeeQuantity, double employeeBaseSalary, Address address, Date fundationDate, Date licenseDate) {
+        super(name, employeeQuantity, employeeBaseSalary, address, fundationDate);
+        this.licenseDate = licenseDate;
+    }
+
+    public Date getLicenseDate() { return licenseDate; }
+    public void setLicenseDate(Date licenseDate) { this.licenseDate = licenseDate; }
+
+    @Override
+    public String toString() {
+        return super.toString() + "Food { licenseDate: " + licenseDate + " }";
+    }
+}
