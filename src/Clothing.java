79b2488ed4b6805d7eeb27072e771294e1338523
Gabriel Franca <gabrielgfranca7@gmail.com>
2025-10-05 19:38:05 -0300

    Clothing Class

---
+++ src/Clothing.java
@@ -0,0 +1,21 @@
+public class Clothing extends Store {
+    private boolean ImportedProducts;
+
+    public Clothing(String name, int employeeQuantity, Address address, Date fundationDate, boolean ImportedProducts) {
+        super(name, employeeQuantity, address, fundationDate);
+        this.ImportedProducts = ImportedProducts;
+    }
+
+    public Clothing(String name, int employeeQuantity, double employeeBaseSalary, Address address, Date fundationDate, boolean ImportedProducts) {
+        super(name, employeeQuantity, employeeBaseSalary, address, fundationDate);
+        this.ImportedProducts = ImportedProducts;
+    }
+
+    public boolean getImportedProducts() { return ImportedProducts; }
+    public void setImportedProducts(boolean ImportedProducts) { this.ImportedProducts = ImportedProducts; }
+
+    @Override
+    public String toString() {
+        return super.toString() + ", ImportedProducts: " + ImportedProducts;
+    }
+}
