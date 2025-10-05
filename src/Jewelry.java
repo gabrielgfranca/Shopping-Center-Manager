639996baf881e3762c4ef4f1670f29d065448637
Gabriel Franca <gabrielgfranca7@gmail.com>
2025-10-05 19:42:14 -0300

    Jewelry Class

---
+++ src/Jewelry.java
@@ -0,0 +1,21 @@
+public class Jewelry extends Store{
+    private double monthlySalesGoal;
+
+    public Jewelry(String name, int employeeQuantity, Address address, Date fundationDate, double monthlySalesGoal) {
+        super(name, employeeQuantity, address, fundationDate);
+        this.monthlySalesGoal = monthlySalesGoal;
+    }
+
+    public Jewelry(String name, int employeeQuantity, double employeeBaseSalary, Address address, Date fundationDate, double monthlySalesGoal) {
+        super(name, employeeQuantity, employeeBaseSalary, address, fundationDate);
+        this.monthlySalesGoal = monthlySalesGoal;
+    }
+
+    public double getMonthlySalesGoal() { return monthlySalesGoal; }
+    public void setMonthlySalesGoal(double monthlySalesGoal) { this.monthlySalesGoal = monthlySalesGoal; }
+
+    @Override
+    public String toString() {
+        return super.toString() + ", Monthly Sales Goal: " + monthlySalesGoal;
+    }
+}
