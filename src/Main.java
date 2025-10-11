f167aa76344bb6bf0a7d52b830b02e372d1abd0b
Gabriel Franca <gabrielgfranca7@gmail.com>
2025-10-11 15:49:39 -0300

    refactor: adapt Main class to use builder methods and updated project structure

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,11 @@
+import models.Address;
+import models.Date;
+import models.Product;
+import models.Store;
 import java.util.Scanner;
+
+import static builders.CreateProduct.createProduct;
+import static builders.CreateStore.createStore;
 
 public class Main {
     public static void main(String[] args) {
@@ -18,10 +25,10 @@ public class Main {
 
             switch (choice) {
                 case 1:
-                    store = createStore();
+                    store = createStore(input);
                     break;
                 case 2:
-                    product = createProduct();
+                    product = createProduct(input);
                     break;
                 case 3:
                     System.out.println("Exiting...");
@@ -43,84 +50,7 @@ public class Main {
         } else {
             System.out.println("The Store and Product needs to be created before the verification.");
         }
+
         input.close();
-    }
-
-    public static Store createStore() {
-        Scanner input = new Scanner(System.in);
-
-        System.out.println("Enter the name of the store");
-        String storeName = input.nextLine();
-
-        System.out.println("Enter the number of employees");
-        int employeeQuantity = input.nextInt();
-        input.nextLine();
-
-        System.out.println("Enter the employee base salary");
-        double employeeBaseSalary = input.nextDouble();
-        input.nextLine();
-
-        System.out.println("Store Address:");
-        System.out.print("Street: ");
-        String streetName = input.nextLine();
-        System.out.print("City: ");
-        String cityName = input.nextLine();
-        System.out.print("State: ");
-        String estateName = input.nextLine();
-        System.out.print("Country: ");
-        String countryName = input.nextLine();
-        System.out.print("Zip code: ");
-        String zipCode = input.nextLine();
-        System.out.print("Number: ");
-        String number = input.nextLine();
-        System.out.print("Complement: ");
-        String complement = input.nextLine();
-
-        Address storeAddress = new Address(streetName, cityName, estateName, countryName, zipCode, number, complement);
-
-        System.out.println("Store Foundation Date:");
-        System.out.print("Day: ");
-        int day = input.nextInt();
-        input.nextLine();
-        System.out.print("Month: ");
-        int month = input.nextInt();
-        input.nextLine();
-        System.out.print("Year: ");
-        int year = input.nextInt();
-        input.nextLine();
-
-        Date foundationDate = new Date(year, month, day);
-        Store finalStore = new Store(storeName, employeeQuantity,  employeeBaseSalary, storeAddress, foundationDate);
-
-        System.out.println("Store created successfully!");
-        return finalStore;
-    }
-
-    public static Product createProduct() {
-        Scanner input = new Scanner(System.in);
-
-        System.out.println("Enter the name of the product");
-        String productName = input.nextLine();
-
-        System.out.println("Enter the product price");
-        double productPrice = input.nextDouble();
-        input.nextLine();
-
-        System.out.println("Product Expiration Date: ");
-        System.out.println("Day: ");
-        int day = input.nextInt();
-        input.nextLine();
-        System.out.println("Month: ");
-        int month = input.nextInt();
-        input.nextLine();
-        System.out.println("Year: ");
-        int year = input.nextInt();
-        input.nextLine();
-
-        Date expirationDate = new Date(year, month, day);
-        Product product = new Product(productName, productPrice, expirationDate);
-
-        System.out.println("Product created successfully!");
-        return product;
     }
 }
